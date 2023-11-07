fad799a9f6d5db352c6ace9ea2f19932eebbc374
Jacob Gonzales <jagonz@live.com>
2023-11-07 15:48:16 -0600

    complete lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,12 @@
-def write_file(file_name, file_content):
-    pass
+def write_file(file_name, file_content = 'This is a test content.'):
+    with open(f'{file_name}.txt', mode='w') as log_file:
+        log_file.write(file_content)
 
-def append_file(file_name, append_content):
-    pass
+def append_file(file_name, append_content='Appended content.'):
+    with open(f'{file_name}.txt', mode='a') as log_file:
+        log_file.write(append_content)
 
 def read_file(file_name):
-    pass
+    with open(f'{file_name}.txt') as file:
+        content = file.read()
+        return content
